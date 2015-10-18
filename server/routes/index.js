d82f6864591e2a796190b7be58c74908a0c17810
sergey <sergnbz1@gmail.com>
2015-10-18 11:56:45 +0300

    routing improved

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var config = require('config');
+var express = require('express');
+var router = express.Router(config.routerOptions);
+var fs = require('fs');
+var path = require('path');
+var basename = path.basename(module.filename);
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename);
+  })
+  .forEach(function(file) {
+    if (file.slice(-3) !== '.js') return;
+    var route = require(path.join(__dirname, file));
+    router.use(route);
+  });
 
 module.exports = router;
